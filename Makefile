da3d5c4869cb30160aa7ef2715b86b9f14f4dfb8
Quasar32 <academytown@gmail.com>
2022-09-20 09:22:49 -0500

    Added GLAD

--- Makefile
+++ Makefile
@@ -1,8 +1,35 @@
-CDFLAGS = -DUNICODE 
-LDFLAGS = -municode -fno-exceptions -fno-rtti
+CC = gcc
+CXX = g++
 
-output:
+CXXFLAGS = -DUNICODE -Wall 
+CXXFLAGS += -Ilib/glad/include -Ilib/wgl
+CXXFLAGS += -MT $@ -MMD -MP -MF $*.d
+
+LDFLAGS = -municode -fno-exceptions -fno-rtti
+LDFLAGS += -lopengl32 -mwindows
+LDFLAGS += lib/glad/src/glad.o
+
+SRC = $(wildcard src/*.cpp) 
+OBJ = $(SRC:.cpp=.o) 
+BIN = bin
+
+all: libs dirs engine
+
+libs:
 	if not exist bin \
 		mkdir bin
+	if not exist lib/glad/src/glad.o \
+		cd lib/glad && $(CC) -o src/glad.o -Iinclude -c src/glad.c
 
-	g++ src/main.cpp $(CDFLAG) -o bin/engine.exe $(LDFLAGS) 
+dirs:
+	if not exist $(BIN) \
+		mkdir $(BIN)
+
+%.o: %.cpp %.d
+	$(CXX) $(CXXFLAGS) -o $@ -c $<
+
+DEPFILES := $(SRC:%.cpp=%.d)
+include $(wildcard $DEPFILES)
+
+engine: $(OBJ)
+	$(CXX) -o $(BIN)/engine.exe $^ $(LDFLAGS) 
--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,57 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+#define WIN32_LEAN_AND_MEAN
+#define WIN_32_EXTRA_LEAN
 #include <windows.h>
+
+#include <glad/glad.h>
+#include <wglext.h>
 
 #define CLIENT_WIDTH 640
 #define CLIENT_HEIGHT 480
 
 static HINSTANCE g_ins;
 static HWND g_wnd;
+static HDC g_hdc;
+static HGLRC g_glrc;
+
+static PFNWGLCREATECONTEXTATTRIBSARBPROC wglCreateContextAttribsARB;
+
+static void cd_parent(LPWSTR path)
+{
+	WCHAR *find;
+
+	find = wcsrchr(path, '\\');
+	if (find) {
+		*find = '\0';
+	}
+}
+
+static void set_default_directory(void)
+{
+	WCHAR path[MAX_PATH];
+
+	GetModuleFileName(NULL, path, MAX_PATH);
+	cd_parent(path);
+	cd_parent(path);
+	SetCurrentDirectory(path);
+}
 
 static LRESULT __stdcall wnd_proc(HWND wnd, UINT msg, WPARAM wp, LPARAM lp)
 {
 	switch (msg) {
 	case WM_DESTROY:
 		PostQuitMessage(0);
+		return 0;
+	case WM_SIZE:
+		glViewport(0, 0, LOWORD(lp), HIWORD(lp));
+		return 0;
+	case WM_PAINT:
+		glClear(GL_COLOR_BUFFER_BIT);  
+		glClearColor(0.0F, 1.0F, 0.0F, 1.0F);  
+		SwapBuffers(g_hdc);
 		return 0;
 	}
 	return DefWindowProc(wnd, msg, wp, lp);
@@ -26,7 +65,7 @@ static void create_main_window(void)
 	int height;
 
 	ZeroMemory(&wc, sizeof(wc));
-	wc.style = CS_VREDRAW | CS_HREDRAW; 
+	wc.style = CS_VREDRAW | CS_HREDRAW | CS_OWNDC; 
 	wc.lpfnWndProc = wnd_proc;
 	wc.hInstance = g_ins;
 	wc.hCursor = LoadCursor(NULL, IDC_ARROW); 
@@ -55,13 +94,62 @@ static void create_main_window(void)
 				L"Error", MB_ICONERROR);
 		exit(1);
 	}
+}
+	
 
-	ShowWindow(g_wnd, SW_SHOW);
+
+
+static void init_open_gl(void)
+{
+	static const int attrib_list[] = {
+		WGL_CONTEXT_MAJOR_VERSION_ARB, 
+		3,
+		WGL_CONTEXT_MINOR_VERSION_ARB, 
+		3,
+		WGL_CONTEXT_FLAGS_ARB, 
+		0,
+		WGL_CONTEXT_PROFILE_MASK_ARB, 
+		WGL_CONTEXT_CORE_PROFILE_BIT_ARB, 
+		0 
+	};
+
+	PIXELFORMATDESCRIPTOR pfd;
+	int fmt;
+	HGLRC tmp;
+
+	g_hdc = GetDC(g_wnd);
+
+	ZeroMemory(&pfd, sizeof(pfd));
+	pfd.nSize = sizeof(pfd);
+	pfd.nVersion = 1;
+	pfd.dwFlags = PFD_DOUBLEBUFFER |  
+		PFD_DRAW_TO_WINDOW | PFD_SUPPORT_OPENGL;
+	pfd.iPixelType = PFD_TYPE_RGBA;
+	pfd.cColorBits = 32;
+	pfd.cDepthBits = 32;
+
+	fmt = ChoosePixelFormat(g_hdc, &pfd); 
+	SetPixelFormat(g_hdc, fmt, &pfd);  
+
+	tmp = wglCreateContext(g_hdc);
+	wglMakeCurrent(g_hdc, tmp);
+
+	wglCreateContextAttribsARB = (PFNWGLCREATECONTEXTATTRIBSARBPROC) 
+		wglGetProcAddress("wglCreateContextAttribsARB");
+	g_glrc = wglCreateContextAttribsARB(g_hdc, 0, attrib_list);	
+
+	wglMakeCurrent(NULL, NULL);
+	wglDeleteContext(tmp);
+	wglMakeCurrent(g_hdc, g_glrc);
+
+	gladLoadGL();
 }
 
 static void msg_loop(void)
 {
 	MSG msg; 
+
+	ShowWindow(g_wnd, SW_SHOW);
 
 	while (GetMessage(&msg, NULL, 0, 0)) {
 		TranslateMessage(&msg);
@@ -76,7 +164,9 @@ int __stdcall wWinMain(HINSTANCE ins, HINSTANCE prev, LPWSTR cmd, int show)
 	UNREFERENCED_PARAMETER(show);
 
 	g_ins = ins;
+	set_default_directory();
 	create_main_window();
+	init_open_gl();
 	msg_loop();
 	
 	return 0;
